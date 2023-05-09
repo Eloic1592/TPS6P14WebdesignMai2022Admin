5ace220c4b8674a31c67d1e56008c4592a3d5fbf
Eloic <99272320+Eloic1592@users.noreply.github.com>
2023-05-09 08:53:32 +0300

    Update ajoutarticle.blade.php

--- resources/views/ajoutarticle.blade.php
+++ resources/views/ajoutarticle.blade.php
@@ -13,48 +13,6 @@
     </div>
 
 
-    <p><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#largeModal"><a style="color:white;">Rechercher un article</a></button>
-    {{-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#largeModal2"><a style="color:white;">Ajouter un article</a></button></p> --}}
-
-
-    <div class="modal fade" id="largeModal" tabindex="-1">
-                <div class="modal-dialog modal-lg">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title">Rechercher un article</h5>
-                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                    <form class="row g-3" action="{{ url('Article/recherchearticle') }}" method="POST">
-                    @csrf
-                    <div class="col-md-6">
-                            <div class="form-floating">
-                            <input type="text" name="categorie" class="form-control" id="floatingZip" placeholder="Categorie de l' article"  />
-                                <label for="floatingZip">Categorie du  article</label>
-                            </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-floating">
-                        <input type="text" name="titre" class="form-control" id="floatingZip" placeholder="Titre de l' article"  />
-                            <label for="floatingZip">Titre de l' article</label>
-                        </div>
-                    </div>
-                    <div class="col-md-12">
-                        <div class="form-floating">
-                        <input type="text" name="contenu" class="form-control" id="floatingZip" placeholder="Contenu de  l' article"  />
-                            <label for="floatingZip">Contenu de  l' article</label>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
-                      <input type="submit" class="btn btn-primary" value="Valider">
-                    </div>
-                </form>
-                  </div>
-                </div>
-            </div><!-- End Large Modal-->
-    </div>
-
     {{-- Content --}}
     <section class="section">
         <div class="row">
